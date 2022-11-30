d990949e9cafbc22399f604caf5389f481d16e9a
matmello <mat.mello93@gmail.com>
2022-11-30 00:39:01 -0300

    Adding environment variable usage as example

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,8 @@
+export const mode = process.env.NODE_ENV as
+  | "development"
+  | "staging"
+  | "production";
+
 export const SkipifyClassNames = {
   emailInput: "_SKIPIFY_email_input",
   paymentButton: "_SKIPIFY_payment_button",
