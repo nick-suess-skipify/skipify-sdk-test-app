6668fef817fdccc9f5160ec34f5454573fa4bdb9
Matheus Mello <mat.mello93@gmail.com>
2024-11-11 12:56:49 -0300

    Switch to new staging (#240)

--- packages/shared/src/environments/environment.stage.ts
+++ packages/shared/src/environments/environment.stage.ts
@@ -1,10 +1,10 @@
 export const environment = {
   SDK_VERSION: '0.10.0',
   NODE_ENV: 'production',
-  MERCHANT_SERVICE_URL: 'https://stage-merchants.skipify.com',
+  MERCHANT_SERVICE_URL: 'https://merchants.staging.skipify.com',
   SDK_URL: 'https://stagecdn.skipify.com/sdk',
   IFRAME_ORIGIN: 'http://localhost:3002',
-  SKIPIFY_CHECKOUT_URL: 'https://stagecheckout.skipify.com',
-  SIMPLE_CHECKOUT_URL: 'https://simple.stage.skipify.com',
+  SKIPIFY_CHECKOUT_URL: 'https://checkout.staging.skipify.com',
+  SIMPLE_CHECKOUT_URL: 'https://simple.staging.skipify.com',
   LAUNCHDARKLY_CLIENT_SIDE_ID: '6489e204a817781b1dd22976',
 };
