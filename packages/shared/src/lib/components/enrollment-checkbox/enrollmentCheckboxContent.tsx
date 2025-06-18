1b07d86f0567de0b16cdf721866ed8fa9f14d2df
Mike Martin <100533193+mike-martin-921@users.noreply.github.com>
2025-06-18 15:54:12 -0400

    UNO-230 Enrollment Checkbox component updates (#305)

--- packages/shared/src/lib/components/enrollment-checkbox/enrollmentCheckboxContent.tsx
+++ packages/shared/src/lib/components/enrollment-checkbox/enrollmentCheckboxContent.tsx
@@ -19,84 +19,128 @@ export const SkipifyEnrollmentCheckboxContent: React.FC = (props) => {
     };
 
     return (
-        <Container>
-            <Checkbox checked={checked} onChange={() => handleCheckbox()} darkMode={darkMode} />
-            <Content>
-                <ContentTitle darkMode={darkMode}>Save my details for future checkouts</ContentTitle>
-                <ContentDescription darkMode={darkMode}>
-                    Save your contact information and payment method to breeze through checkouts that are powered by
-                    Skipify.
-                </ContentDescription>
-                <Terms darkMode={darkMode}>
+        <Container darkMode={darkMode} checked={checked} onClick={handleCheckbox}>
+            <InnerContainer>
+                <CheckboxRow>
+                    <CheckboxArea>
+                        <Checkbox 
+                            checked={checked} 
+                            onChange={handleCheckbox}
+                            darkMode={darkMode}
+                            type="checkbox"
+                        />
+                    </CheckboxArea>
+                    <ContentTitle darkMode={darkMode}>Save my details for future checkouts</ContentTitle>
+                </CheckboxRow>
+            </InnerContainer>
+            <TermsContainer>
+                <TermsText darkMode={darkMode}>
                     By continuing, you agree to the Skipify{' '}
                     <Link href="https://www.skipify.com/us/terms-and-conditions/" darkMode={darkMode}>
                         Terms & Conditions
                     </Link>{' '}
                     and{' '}
                     <Link href="https://www.skipify.com/us/privacy-policy/" darkMode={darkMode}>
-                        Privacy Policy
+                        Privacy Policy.
                     </Link>
-                </Terms>
-            </Content>
+                </TermsText>
+            </TermsContainer>
         </Container>
     );
 };
 
-const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    font-family: Poppins;
-    border-radius: 5px;
-    border: 1px solid #01ead3;
-    padding: 16px 18px;
-`;
-
-const Checkbox = styled.input.attrs(() => ({
-    type: 'checkbox',
-}))<{ darkMode: boolean }>`
-    width: 17px;
-    height: 17px;
-    cursor: pointer;
-    flex-shrink: 0;
-    ${(props) =>
-        props.darkMode
-            ? `accent-color: ${props.checked ? '#01EAD3' : '#FFFFFF'};`
-            : `accent-color: ${props.checked ? '#000000' : '#FFFFFF'};`}
-`;
-
-const Content = styled.div`
-    display: flex;
+const Container = styled.div<{ darkMode: boolean; checked: boolean }>`
+    width: 100%;
+    padding: 4px 4px 16px 4px;
+    background: ${(props) => (props.darkMode ? '#1E1E1E' : 'white')};
+    border-radius: 8px;
+    outline: 1px solid ${(props) => {
+        if (props.checked) {
+            return '#01EAD3';
+        }
+        return props.darkMode ? '#404040' : '#E0E0E0';
+    }};
+    outline-offset: -1px;
     flex-direction: column;
-    margin-left: 18px;
+    justify-content: flex-start;
+    align-items: flex-start;
+    display: flex;
+    font-family: Poppins;
+    box-sizing: border-box;
+    cursor: pointer;
+`;
+
+const InnerContainer = styled.div`
+    align-self: stretch;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 4px;
+    display: flex;
+`;
+
+const CheckboxRow = styled.div`
+    align-self: stretch;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 4px;
+    display: flex;
+`;
+
+const CheckboxArea = styled.div`
+    padding: 12px;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 8px;
+    display: flex;
+`;
+
+const Checkbox = styled.input<{ darkMode: boolean }>`
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+    accent-color: ${(props) => (props.darkMode ? '#01EAD3' : '#1F7F79')};
 `;
 
 const ContentTitle = styled.div<{ darkMode: boolean }>`
-    color: ${(props) => (props.darkMode ? '#FFFFFF' : '#000000')};
-    font-weight: 600;
-    font-size: 12px;
+    flex: 1 1 0;
+    color: ${(props) => (props.darkMode ? '#FFFFFF' : '#1E1E1E')};
+    font-size: 14px;
     font-family: Poppins;
-    margin-bottom: 5px;
+    font-weight: 400;
+    line-height: 18px;
+    word-wrap: break-word;
 `;
 
-const ContentDescription = styled.div<{ darkMode: boolean }>`
-    color: ${(props) => (props.darkMode ? '#FFFFFF' : '#000000')};
-    font-weight: 400;
-    font-size: 12px;
-    font-family: Poppins;
-    margin-bottom: 16px;
+const TermsContainer = styled.div`
+    align-self: stretch;
+    padding-left: 12px;
+    padding-right: 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    display: flex;
 `;
 
-const Terms = styled.div<{ darkMode: boolean }>`
-    color: ${(props) => (props.darkMode ? '#B2B2B2' : '#7a7a7a')};
-    font-weight: 400;
-    font-size: 10px;
+const TermsText = styled.div<{ darkMode: boolean }>`
+    flex: 1 1 0;
+    color: ${(props) => (props.darkMode ? '#B2B2B2' : '#737373')};
+    font-size: 12px;
     font-family: Poppins;
+    font-weight: 400;
+    line-height: 16px;
+    word-wrap: break-word;
 `;
 
 const Link = styled.a.attrs({
     target: '_blank',
 })<{ darkMode: boolean }>`
-    color: ${(props) => (props.darkMode ? '#01EAD3' : '#1f7f79')};
+    color: ${(props) => (props.darkMode ? '#01EAD3' : '#1F7F79')};
+    font-weight: 600;
     cursor: pointer;
-    text-decoration: underline;
+    text-decoration: none;
+    
+    &:hover {
+        text-decoration: underline;
+    }
 `;
